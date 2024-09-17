6b0dbd8ce7b8b53d5f4a0ffcd955f5c5e845f026
priyankaforu <priyankapudi4u@gmail.com>
2024-09-17 11:36:28 +0000

    Modified articles and writings

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
 from flask import Flask, render_template
+from collections import defaultdict
 
 app = Flask(__name__)
 
+ARTICLES = [
+    {
+        'id': 1,
+        'title' : 'Transformation of UX design',
+        'Genre' : 'Tech & Design',
+        'year'  : 2024
+    },
+    {
+        'id': 2,
+        'title' : 'The stoic philosophy in leading adventurous life',
+        'Genre' : 'Philosophy',
+        'year'  : 2023
+    },
+    {
+        'id': 3,
+        'title' : 'Machine learning modules',
+        'Genre' : 'ML and AI',
+        'year'  : 2024
+    }
+
+]
+
 @app.route('/')
-def hello_priyanks():
-    return render_template('home.html')
+def hello_priyanka():
+    articles_by_year = defaultdict(list)
+    for article in ARTICLES:
+        articles_by_year[article['year']].append(article)
+    sorted_years = sorted(articles_by_year.keys(), reverse=True)
+    return render_template('home.html', articles_by_year=articles_by_year, years=sorted_years)
 
 print(__name__)
 if __name__ == '__main__':
