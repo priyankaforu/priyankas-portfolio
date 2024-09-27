6893d69db82abd0265dc7e41a87cc24107d35b17
priyankaforu <priyankapudi4u@gmail.com>
2024-09-27 23:24:25 +0530

    Update menu.js

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,1 +1,9 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const mobileMenuItems = document.querySelector('.mobile-menu-items');
+    
+    menuToggle.addEventListener('click', function(e) {
+        e.preventDefault(); // Prevent default button behavior
+        mobileMenuItems.classList.toggle('active');
+    });
+});
